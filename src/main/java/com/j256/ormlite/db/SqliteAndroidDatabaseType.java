0bc901e61dddb20e1753d297f5a32d4ffb752753
graywatson <graywatson@05e1a9df-29cb-4752-bae5-e712e3cd3cb9>
2010-09-09 21:42:48 +0000

    copied over from core

---
+++ src/main/java/com/j256/ormlite/db/SqliteAndroidDatabaseType.java
@@ -0,0 +1,63 @@
+package com.j256.ormlite.db;
+
+import com.j256.ormlite.field.DataType;
+import com.j256.ormlite.field.FieldConverter;
+import com.j256.ormlite.field.FieldType;
+
+/**
+ * Sqlite database type information for the Android OS. This has a difference driver class name.
+ * 
+ * <p>
+ * <b> NOTE: </b> Support for Android is now native. See the section on the manual about running with Android.
+ * </p>
+ * 
+ * <p>
+ * <b> WARNING:</b> If you do endeavor to use JDBC under Android, understand that as of 8/2010, JDBC is currently
+ * <i>unsupported</i>. This may change in the future but until that time, you should use this with caution. You may want
+ * to try the {@link SqlDroidDatabaseType} if this driver does not work for you.
+ * </p>
+ * 
+ * @author graywatson
+ */
+public class SqliteAndroidDatabaseType extends SqliteDatabaseType implements DatabaseType {
+
+	public SqliteAndroidDatabaseType() {
+	}
+
+	@Override
+	public void loadDriver() throws ClassNotFoundException {
+		// Hang out. Nothing to do.
+	}
+
+	@Override
+	public String getDriverUrlPart() {
+		return null;
+	}
+
+	@Override
+	public String getDriverClassName() {
+		return null;
+	}
+
+	@Override
+	protected void appendDateType(StringBuilder sb, int fieldWidth) {
+		// default is to store the date as a string
+		appendDateStringType(sb, fieldWidth);
+	}
+
+	@Override
+	protected void appendBooleanType(StringBuilder sb) {
+		appendShortType(sb);
+	}
+
+	@Override
+	public FieldConverter getFieldConverter(FieldType fieldType) {
+		// we are only overriding certain types
+		switch (fieldType.getDataType()) {
+			case JAVA_DATE :
+				return DataType.JAVA_DATE_STRING;
+			default :
+				return super.getFieldConverter(fieldType);
+		}
+	}
+}
