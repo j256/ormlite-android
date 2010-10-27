4dbd24faca657144c478d4d43c02d81b2754baa8
graywatson <graywatson@05e1a9df-29cb-4752-bae5-e712e3cd3cb9>
2010-10-27 22:05:14 +0000

    Removed the whole concept of the InitConnectionSource.  Now we save the connection to the database and clear it during initialization.  Fixes bug #3096875.

--- src/main/java/com/j256/ormlite/android/AndroidConnectionSource.java
+++ src/main/java/com/j256/ormlite/android/AndroidConnectionSource.java
@@ -1,32 +1,84 @@
 package com.j256.ormlite.android;
 
-import android.database.sqlite.SQLiteDatabase;
+import java.sql.SQLException;
+
 import android.database.sqlite.SQLiteOpenHelper;
 
 import com.j256.ormlite.android.apptools.OrmLiteSqliteOpenHelper;
+import com.j256.ormlite.db.DatabaseType;
+import com.j256.ormlite.db.SqliteAndroidDatabaseType;
+import com.j256.ormlite.support.ConnectionSource;
+import com.j256.ormlite.support.DatabaseConnection;
 
 /**
- * Main connection source. Uses the standrd android SQLiteOpenHelper. For best results, use our helper,
+ * Main connection source. Uses the standard Android SQLiteOpenHelper. For best results, use our helper,
  * 
  * @see OrmLiteSqliteOpenHelper
  * 
  * @author kevingalligan, graywatson
  */
-public class AndroidConnectionSource extends BaseAndroidConnectionSource {
+public class AndroidConnectionSource implements ConnectionSource {
 
 	private SQLiteOpenHelper helper;
+	private DatabaseConnection readOnlyConnection = null;
+	private DatabaseConnection readWriteConnection = null;
+	private DatabaseType databaseType = new SqliteAndroidDatabaseType();
+	private boolean useThreadLocal = false;
+	private final ThreadLocal<DatabaseConnection> savedConnection = new ThreadLocal<DatabaseConnection>();
 
 	public AndroidConnectionSource(SQLiteOpenHelper helper) {
 		this.helper = helper;
 	}
 
-	@Override
-	protected SQLiteDatabase getReadOnlyDatabase() {
-		return helper.getReadableDatabase();
+	public DatabaseConnection getReadOnlyConnection() throws SQLException {
+		if (useThreadLocal) {
+			DatabaseConnection conn = savedConnection.get();
+			if (conn != null) {
+				return conn;
+			}
+		}
+		if (readOnlyConnection == null) {
+			readOnlyConnection = new AndroidDatabaseConnection(helper.getReadableDatabase(), false);
+		}
+		return readOnlyConnection;
 	}
 
-	@Override
-	protected SQLiteDatabase getReadWriteDatabase() {
-		return helper.getWritableDatabase();
+	public DatabaseConnection getReadWriteConnection() throws SQLException {
+		if (useThreadLocal) {
+			DatabaseConnection conn = savedConnection.get();
+			if (conn != null) {
+				return conn;
+			}
+		}
+		if (readWriteConnection == null) {
+			readWriteConnection = new AndroidDatabaseConnection(helper.getWritableDatabase(), true);
+		}
+		return readWriteConnection;
+	}
+
+	public void releaseConnection(DatabaseConnection connection) throws SQLException {
+		// noop since connection management is handled by AndroidOS
+	}
+
+	public void saveSpecialConnection(DatabaseConnection connection) {
+		/*
+		 * We can't just set the read-only and read-write connections to be the connection since it is not synchronized
+		 * and other threads may not get the reset in the clear method.
+		 */
+		useThreadLocal = true;
+		savedConnection.set(connection);
+	}
+
+	public void clearSpecialConnection(DatabaseConnection connection) {
+		useThreadLocal = false;
+		savedConnection.set(null);
+	}
+
+	public void close() {
+		// the helper is closed so it calls close here, so this CANNOT be a call back to helper.close()
+	}
+
+	public DatabaseType getDatabaseType() {
+		return databaseType;
 	}
 }
--- src/main/java/com/j256/ormlite/android/apptools/OrmLiteSqliteOpenHelper.java
+++ src/main/java/com/j256/ormlite/android/apptools/OrmLiteSqliteOpenHelper.java
@@ -1,39 +1,61 @@
 package com.j256.ormlite.android.apptools;
 
-import java.sql.SQLException;
-
 import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 import android.database.sqlite.SQLiteDatabase.CursorFactory;
 
 import com.j256.ormlite.android.AndroidConnectionSource;
-import com.j256.ormlite.android.BaseAndroidConnectionSource;
+import com.j256.ormlite.android.AndroidDatabaseConnection;
 import com.j256.ormlite.support.ConnectionSource;
 
 /**
- * Sqlite database open helper which can be extended by your application.
+ * SQLite database open helper which can be extended by your application to help manage when the application needs to
+ * create or upgrade its database.
  * 
  * @author kevingalligan
  */
 public abstract class OrmLiteSqliteOpenHelper extends SQLiteOpenHelper {
 
-	private AndroidConnectionSource connectionSource;
+	private AndroidConnectionSource connectionSource = new AndroidConnectionSource(this);
 
 	public OrmLiteSqliteOpenHelper(Context context, String databaseName, CursorFactory factory, int databaseVersion) {
 		super(context, databaseName, factory, databaseVersion);
-		connectionSource = new AndroidConnectionSource(this);
 	}
 
 	/**
 	 * What to do when your database needs to be created. Usually this entails creating the tables and loading any
 	 * initial data.
+	 * 
+	 * <p>
+	 * <b>NOTE:</b> You should use the connectionSource argument that is passed into this method call or the one
+	 * returned by getConnectionSource(). If you use your own, a recursive call or other unexpected results may result.
+	 * </p>
+	 * 
+	 * @param database
+	 *            Database being created.
+	 * @param connectionSource
+	 *            To use get connections to the database to be created.
 	 */
 	public abstract void onCreate(SQLiteDatabase database, ConnectionSource connectionSource);
 
 	/**
 	 * What to do when your database needs to be updated. This could mean careful migration of old data to new data.
 	 * Maybe adding or deleting database columns, etc..
+	 * 
+	 * <p>
+	 * <b>NOTE:</b> You should use the connectionSource argument that is passed into this method call or the one
+	 * returned by getConnectionSource(). If you use your own, a recursive call or other unexpected results may result.
+	 * </p>
+	 * 
+	 * @param database
+	 *            Database being upgraded.
+	 * @param connectionSource
+	 *            To use get connections to the database to be updated.
+	 * @param oldVersion
+	 *            The version of the current database so we can know what to do to the database.
+	 * @param newVersion
+	 *            The version that we are upgrading the database to.
 	 */
 	public abstract void onUpgrade(SQLiteDatabase database, ConnectionSource connectionSource, int oldVersion,
 			int newVersion);
@@ -50,7 +72,19 @@ public abstract class OrmLiteSqliteOpenHelper extends SQLiteOpenHelper {
 	 */
 	@Override
 	public final void onCreate(SQLiteDatabase db) {
-		onCreate(db, new InitConnectionSource(db));
+		ConnectionSource cs = getConnectionSource();
+		/*
+		 * The method is called by Android database helper's get-database calls when Android detects that we need to
+		 * create or update the database. So we have to use the database argument and save a connection to it on the
+		 * AndroidConnectionSource, otherwise it will go recursive if the subclass calls getConnectionSource().
+		 */
+		AndroidDatabaseConnection conn = new AndroidDatabaseConnection(db, true);
+		try {
+			cs.saveSpecialConnection(conn);
+			onCreate(db, cs);
+		} finally {
+			cs.clearSpecialConnection(conn);
+		}
 	}
 
 	/**
@@ -58,7 +92,19 @@ public abstract class OrmLiteSqliteOpenHelper extends SQLiteOpenHelper {
 	 */
 	@Override
 	public final void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
-		onUpgrade(db, new InitConnectionSource(db), oldVersion, newVersion);
+		ConnectionSource cs = getConnectionSource();
+		/*
+		 * The method is called by Android database helper's get-database calls when Android detects that we need to
+		 * create or update the database. So we have to use the database argument and save a connection to it on the
+		 * AndroidConnectionSource, otherwise it will go recursive if the subclass calls getConnectionSource().
+		 */
+		AndroidDatabaseConnection conn = new AndroidDatabaseConnection(db, true);
+		try {
+			cs.saveSpecialConnection(conn);
+			onUpgrade(db, cs, oldVersion, newVersion);
+		} finally {
+			cs.clearSpecialConnection(conn);
+		}
 	}
 
 	/**
@@ -69,33 +115,8 @@ public abstract class OrmLiteSqliteOpenHelper extends SQLiteOpenHelper {
 		super.close();
 		try {
 			connectionSource.close();
-		} catch (SQLException e) {
-			// ignore the exception since the helper doesn't throw it
 		} finally {
 			connectionSource = null;
-		}
-	}
-
-	/**
-	 * Internal connection source used to avoid recursion when initializing the databases. If we use the
-	 * {@link AndroidConnectionSource} which uses the helper, it ends up calling itself.
-	 */
-	private class InitConnectionSource extends BaseAndroidConnectionSource {
-
-		private SQLiteDatabase db;
-
-		public InitConnectionSource(SQLiteDatabase db) {
-			this.db = db;
-		}
-
-		@Override
-		protected SQLiteDatabase getReadOnlyDatabase() {
-			return db;
-		}
-
-		@Override
-		protected SQLiteDatabase getReadWriteDatabase() {
-			return db;
 		}
 	}
 }
