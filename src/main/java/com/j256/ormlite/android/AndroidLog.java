09b3d5982c0d28fb6d3fee924fb2dff46fd312aa
graywatson <graywatson@05e1a9df-29cb-4752-bae5-e712e3cd3cb9>
2011-01-30 20:03:00 +0000

    Fixed a commons logging bug.

--- src/main/java/com/j256/ormlite/android/AndroidLog.java
+++ src/main/java/com/j256/ormlite/android/AndroidLog.java
@@ -18,75 +18,78 @@ public class AndroidLog implements com.j256.ormlite.logger.Log {
 		this.className = LoggerFactory.getSimpleClassName(className);
 	}
 
-	public boolean isTraceEnabled() {
-		return Log.isLoggable(className, Log.VERBOSE);
+	public boolean isLevelEnabled(Level level) {
+		return Log.isLoggable(className, levelToJavaLevel(level));
 	}
 
-	public boolean isDebugEnabled() {
-		return Log.isLoggable(className, Log.DEBUG);
+	public void log(Level level, String msg) {
+		switch (level) {
+			case TRACE :
+				Log.v(className, msg);
+				break;
+			case DEBUG :
+				Log.d(className, msg);
+				break;
+			case INFO :
+				Log.i(className, msg);
+				break;
+			case WARNING :
+				Log.w(className, msg);
+				break;
+			case ERROR :
+				Log.e(className, msg);
+				break;
+			case FATAL :
+				Log.e(className, msg);
+				break;
+			default :
+				Log.i(className, msg);
+				break;
+		}
 	}
 
-	public boolean isInfoEnabled() {
-		return Log.isLoggable(className, Log.INFO);
+	public void log(Level level, String msg, Throwable t) {
+		switch (level) {
+			case TRACE :
+				Log.v(className, msg, t);
+				break;
+			case DEBUG :
+				Log.d(className, msg, t);
+				break;
+			case INFO :
+				Log.i(className, msg, t);
+				break;
+			case WARNING :
+				Log.w(className, msg, t);
+				break;
+			case ERROR :
+				Log.e(className, msg, t);
+				break;
+			case FATAL :
+				Log.e(className, msg, t);
+				break;
+			default :
+				Log.i(className, msg, t);
+				break;
+		}
 	}
 
-	public boolean isWarnEnabled() {
-		return Log.isLoggable(className, Log.WARN);
-	}
-
-	public boolean isErrorEnabled() {
-		return Log.isLoggable(className, Log.ERROR);
-	}
-
-	public boolean isFatalEnabled() {
-		return Log.isLoggable(className, Log.ERROR);
-	}
-
-	public void trace(String message) {
-		Log.v(className, message);
-	}
-
-	public void trace(String message, Throwable t) {
-		Log.v(className, message, t);
-	}
-
-	public void debug(String message) {
-		Log.d(className, message);
-	}
-
-	public void debug(String message, Throwable t) {
-		Log.d(className, message, t);
-	}
-
-	public void info(String message) {
-		Log.i(className, message);
-	}
-
-	public void info(String message, Throwable t) {
-		Log.i(className, message, t);
-	}
-
-	public void warn(String message) {
-		Log.w(className, message);
-	}
-
-	public void warn(String message, Throwable t) {
-		Log.w(className, message, t);
-	}
-
-	public void error(String message) {
-		Log.e(className, message);
-	}
-
-	public void error(String message, Throwable t) {
-		Log.e(className, message, t);
-	}
-
-	public void fatal(String message) {
-		Log.e(className, message);
-	}
-
-	public void fatal(String message, Throwable t) {
-		Log.e(className, message, t);
+	private int levelToJavaLevel(com.j256.ormlite.logger.Log.Level level) {
+		switch (level) {
+			case TRACE :
+				return Log.VERBOSE;
+			case DEBUG :
+				return Log.DEBUG;
+			case INFO :
+				return Log.INFO;
+			case WARNING :
+				return Log.WARN;
+			case ERROR :
+				return Log.ERROR;
+			case FATAL :
+				return Log.ERROR;
+			default :
+				return Log.INFO;
+		}
 	}
 }
