595bc6f418141219e6b3db2c50262136c41d00c2
graywatson <graywatson@05e1a9df-29cb-4752-bae5-e712e3cd3cb9>
2011-01-30 20:05:18 +0000

    Added missing ListActivity base class.

---
+++ src/main/java/com/j256/ormlite/android/apptools/OrmLiteBaseListActivity.java
@@ -0,0 +1,63 @@
+package com.j256.ormlite.android.apptools;
+
+import android.app.ListActivity;
+import android.content.Context;
+
+import com.j256.ormlite.support.ConnectionSource;
+
+/**
+ * Base class to use for Tab activities in Android.
+ * 
+ * For more information, see {@link OrmLiteBaseActivity}.
+ * 
+ * @author kevingalligan
+ */
+public abstract class OrmLiteBaseListActivity<H extends OrmLiteSqliteOpenHelper> extends ListActivity {
+
+	private H helper;
+
+	/**
+	 * Get a helper for this action.
+	 */
+	public synchronized H getHelper() {
+		if (helper == null) {
+			helper = getHelperInternal(this);
+		}
+		return helper;
+	}
+
+	/**
+	 * Get a connection source for this action.
+	 */
+	public ConnectionSource getConnectionSource() {
+		return getHelper().getConnectionSource();
+	}
+
+	@Override
+	protected void onDestroy() {
+		super.onDestroy();
+		if (helper != null) {
+			OpenHelperManager.release();
+			helper = null;
+		}
+	}
+
+	/**
+	 * @see OrmLiteBaseActivity#getHelperInternal(Context)
+	 */
+	protected H getHelperInternal(Context context) {
+		@SuppressWarnings("unchecked")
+		H newHelper = (H) OpenHelperManager.getHelper(context);
+		return newHelper;
+	}
+
+	/**
+	 * @see OrmLiteBaseActivity#releaseHelper(OrmLiteSqliteOpenHelper)
+	 */
+	protected void releaseHelper(H helper) {
+		if (helper != null) {
+			OpenHelperManager.release();
+			helper = null;
+		}
+	}
+}
