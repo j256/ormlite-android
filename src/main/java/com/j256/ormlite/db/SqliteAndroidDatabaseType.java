a391e536298d9ad8086976fe0c9517cbd13c4890
graywatson <graywatson@05e1a9df-29cb-4752-bae5-e712e3cd3cb9>
2012-09-19 19:03:16 +0000

    Tweaked the OracleDatabaseType per information from stew.

--- src/main/java/com/j256/ormlite/db/SqliteAndroidDatabaseType.java
+++ src/main/java/com/j256/ormlite/db/SqliteAndroidDatabaseType.java
@@ -5,6 +5,7 @@ import java.sql.SQLException;
 import com.j256.ormlite.android.DatabaseTableConfigUtil;
 import com.j256.ormlite.field.DataPersister;
 import com.j256.ormlite.field.FieldConverter;
+import com.j256.ormlite.field.FieldType;
 import com.j256.ormlite.field.types.DateStringType;
 import com.j256.ormlite.misc.VersionUtils;
 import com.j256.ormlite.support.ConnectionSource;
@@ -43,15 +44,15 @@ public class SqliteAndroidDatabaseType extends BaseSqliteDatabaseType implements
 	}
 
 	@Override
-	protected void appendDateType(StringBuilder sb, int fieldWidth) {
+	protected void appendDateType(StringBuilder sb, FieldType fieldType, int fieldWidth) {
 		// default is to store the date as a string
-		appendStringType(sb, fieldWidth);
+		appendStringType(sb, fieldType, fieldWidth);
 	}
 
 	@Override
-	protected void appendBooleanType(StringBuilder sb, int fieldWidth) {
+	protected void appendBooleanType(StringBuilder sb, FieldType fieldType, int fieldWidth) {
 		// we have to convert booleans to numbers
-		appendShortType(sb, fieldWidth);
+		appendShortType(sb, fieldType, fieldWidth);
 	}
 
 	@Override
