51105ee4dd3d55908167dbccff30e1bbcb792868
Gray Watson <gray.github@mailnull.com>
2014-02-01 20:15:27 -0500

    Fixed the string processing when persisting java.sql.Timestamp classes. Bug #163.

--- src/main/java/com/j256/ormlite/db/SqliteAndroidDatabaseType.java
+++ src/main/java/com/j256/ormlite/db/SqliteAndroidDatabaseType.java
@@ -4,9 +4,10 @@ import java.sql.SQLException;
 
 import com.j256.ormlite.android.DatabaseTableConfigUtil;
 import com.j256.ormlite.field.DataPersister;
-import com.j256.ormlite.field.FieldConverter;
 import com.j256.ormlite.field.FieldType;
 import com.j256.ormlite.field.types.DateStringType;
+import com.j256.ormlite.field.types.TimeStampStringType;
+import com.j256.ormlite.field.types.TimeStampType;
 import com.j256.ormlite.support.ConnectionSource;
 import com.j256.ormlite.table.DatabaseTableConfig;
 
@@ -50,13 +51,20 @@ public class SqliteAndroidDatabaseType extends BaseSqliteDatabaseType {
 	}
 
 	@Override
-	public FieldConverter getFieldConverter(DataPersister dataPersister) {
+	public DataPersister getDataPersister(DataPersister defaultPersister) {
+		if (defaultPersister == null) {
+			return super.getDataPersister(null);
+		}
 		// we are only overriding certain types
-		switch (dataPersister.getSqlType()) {
+		switch (defaultPersister.getSqlType()) {
 			case DATE :
-				return DateStringType.getSingleton();
+				if (defaultPersister instanceof TimeStampType) {
+					return TimeStampStringType.getSingleton();
+				} else {
+					return DateStringType.getSingleton();
+				}
 			default :
-				return super.getFieldConverter(dataPersister);
+				return super.getDataPersister(defaultPersister);
 		}
 	}
 
