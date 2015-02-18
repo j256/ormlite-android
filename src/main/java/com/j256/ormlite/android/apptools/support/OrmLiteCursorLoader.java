d6cd5c7cfddcdec08b173e5cff0e343bb56018bf
Gray Watson <gray.github@mailnull.com>
2015-02-18 12:16:37 -0500

    Added back in the support libraries for the cursor loader code.

--- src/main/java/com/j256/ormlite/android/apptools/support/OrmLiteCursorLoader.java
+++ src/main/java/com/j256/ormlite/android/apptools/support/OrmLiteCursorLoader.java
@@ -1,12 +1,12 @@
-package com.j256.ormlite.android.apptools;
+package com.j256.ormlite.android.apptools.support;
 
 import static com.j256.ormlite.stmt.StatementBuilder.StatementType.SELECT;
 
 import java.sql.SQLException;
 
-import android.content.AsyncTaskLoader;
 import android.content.Context;
 import android.database.Cursor;
+import android.support.v4.content.AsyncTaskLoader;
 
 import com.j256.ormlite.android.AndroidCompiledStatement;
 import com.j256.ormlite.dao.Dao;
